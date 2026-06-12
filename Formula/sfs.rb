f8b1530c89dd9febf349b91768959afdbab579ce
채명정 <jack2718@naver.com>
2026-06-12 17:47:21 +0900

    release: sfs 0.8.38 (tar sha 2bea5468)

--- Formula/sfs.rb
+++ Formula/sfs.rb
@@ -7,8 +7,8 @@ class Sfs < Formula
   homepage "https://github.com/MJ-0701/solon-product"
 
   # Replace VERSION and SHA256 when cutting a public release tarball.
-  url "https://github.com/MJ-0701/solon-product/archive/refs/tags/v0.8.37.tar.gz"
-  sha256 "31d31f96f1b352043cc135929af23d0092129c3f398e0797ea40123a443a4d6e"
+  url "https://github.com/MJ-0701/solon-product/archive/refs/tags/v0.8.38.tar.gz"
+  sha256 "2bea54680b5d2f84eb985f292456ccfe321547ba28a71a9be036e4d79d44020a"
 
   def install
     libexec.install Dir["*"]
